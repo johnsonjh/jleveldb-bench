c9584b611dd0bc7b7474a1a9bebe015138ea6f63
rjl493456442 <garyrong0905@gmail.com>
2020-05-28 20:46:33 +0800

    datasets: update benchmark reports

--- cmd/ldb-readbench/ldb-readbench.go
+++ cmd/ldb-readbench/ldb-readbench.go
@@ -138,10 +138,6 @@ var tests = map[string]Benchmarker{
 		BlockCacheCapacity: 100 * opt.MiB,
 		Filter:             filter.NewBloomFilter(10),
 	}},
-	"random-read-metacache-filter": randomRead{Options: opt.Options{
-		MetadataCacheCapacity: 100 * opt.MiB,
-		Filter:                filter.NewBloomFilter(10),
-	}},
 }
 
 func testnames() (n []string) {
